ed5f96c17a800a187e3df4e47d529196828bb0f2
dread93 <Ktyrell93@gmail.com>
2016-04-22 19:31:32 +0000

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+    def change
+        create_table :users do |t|
+            t.string :name
+            t.string :password
+            t.string :email
+            
+            t.timestamps null: false
+        end
+    end
 end
